9eb39c654d97d034003972a5882d09f0f6655919
Mani143k <144652236+Mani143k@users.noreply.github.com>
2023-09-19 14:48:20 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,25 @@
+class Player:
+
+  def play(self):
+
+     print("Theplayerisplayingcricket.")
+
+class Batsman(Player):
+
+  def play(self):
+
+    print("Thebatsmanisbatting.")
+
+class Bowler(Player):
+
+defplay(self):
+
+print("Thebowlerisbowling.")
+
+batsman=Batsman()
+
+bowler=Bowler()
+
+batsman.play()
+
+bowler.play()
